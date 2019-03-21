384053a88c28bca00a0d4d8edb55e0e7d604945a
zhangming.wu <1137152932@qq.com>
2019-03-21 12:00:09 +0800

    使用动态加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 // 1.引入vue，vue-loader
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '../home/index.vue';
+// 动态加载组件
+const Home = () => import('../home/index.vue');
 
 // 2.通过vue.use()明确地安装路由功能
 Vue.use(Router);
