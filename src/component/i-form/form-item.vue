038c98e9b60d6b9ea8af560e6d9ca55b4639e88c
zhangming.wu <1137152932@qq.com>
2019-03-21 15:17:38 +0800

    在form于form-item应用事件分发

--- src/component/i-form/form-item.vue
+++ src/component/i-form/form-item.vue
@@ -7,8 +7,11 @@
   </div>
 </template>
 <script>
+import Emitter from '@/mixins/emitter.js';
+
 export default {
   name: 'FromItem',
+  mixins: [Emitter],
   props: {
     label: {
       type: String,
@@ -17,6 +20,15 @@ export default {
     prop: {
       type: String
     }
+  },
+  mounted () {
+    if (this.prop) {
+      this.dispatch('Form', 'on-form-item-add', this);
+    }
+  },
+  // 组件销毁前，将实例从Form的缓存中移除
+  beforeDestroy () {
+    this.dispatch('Form', 'on-form-item-remove', this);
   }
 }
 </script>
--- src/component/i-form/form.vue
+++ src/component/i-form/form.vue
@@ -7,6 +7,11 @@
 <script>
 export default {
   name: 'Form',
+  data () {
+    return {
+      fields: []
+    }
+  },
   props: {
     rules: {
       type: Object
@@ -14,6 +19,24 @@ export default {
     model: {
       type: Object
     }
+  },
+  created () {
+    /**
+     * 注意： Vue.js的组件渲染顺序是由内而外，所以FormItem要先于Form渲染
+     * 在FormItem的mounted触发时，我们向Form派发事件 on-form-item-add，
+     * 并且将当前FormItem的this传给Form，而此时，Form的mounted尚未触发，因为Form在最外层
+     * 因此不能在mounted里面监听事件，需要在created里面自定义监听事件
+     */
+    this.$on('on-form-item-add', (field) => {
+      if (field) {
+        this.fields.push(field);
+      }
+    });
+    this.$on('on-form-item-remove', (field) => {
+      if (field.prop) {
+        this.fields.splice(this.fields.indexOf(field), 1);
+      }
+    });
   }
 }
 </script>
